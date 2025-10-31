71ac097be18772b7d479523a6d24ae15c7b1082a
dendrocat <dan.re0@mail.ru>
2025-10-31 14:42:53 +0300

    Added keymap to settings

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -1,14 +1,16 @@
---- Terminal
+-- Terminal
 vim.keymap.set('n', '<leader>t', ':tabnew<CR>:terminal<CR>i', { desc = 'Open terminal' })
 vim.keymap.set('t', '<C-n>', "<C-\\><C-n>", { desc = 'Exit terminal' })
 
---- Diagnostic
+-- Diagnostic
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
 
 local jump = function(cnt) vim.diagnostic.jump({ count = cnt, float = true }) end
 vim.keymap.set('n', '[d', function() jump(-1) end, { desc = 'Prev diagnostic' })
 vim.keymap.set('n', ']d', function() jump(1) end, { desc = 'Next diagnostic' })
 
-
---- Format
+-- Format
 vim.keymap.set('n', '==', function() vim.lsp.buf.format({ async = true }) end, { desc = 'Format file' })
+
+-- Settings
+vim.keymap.set('n', '<leader>s', ':tabe $MYVIMRC | cd %:p:h | split . | wincmd k | pwd<CR>', { desc = "Open settings" })
