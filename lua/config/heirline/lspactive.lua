f0c84fe00a5e0ec98f217d8f5243caea4a46a2b0
dendrocat <dan.re0@mail.ru>
2025-11-05 19:35:42 +0300

    Fixed bug with wrong lsp in statusline

--- lua/config/heirline/lspactive.lua
+++ lua/config/heirline/lspactive.lua
@@ -2,9 +2,9 @@ local conditions = require 'heirline.conditions'
 
 local LSPActive = {
 	condition = conditions.lsp_attached,
-	update = { 'LspAttach', 'LspDetach' },
+	update = { 'LspAttach', 'LspDetach', 'BufEnter' },
 	provider = function()
-		return vim.lsp.get_clients({ bufnr = 0 })[1].name
+		return vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })[1].name
 	end,
 	hl = { bold = true },
 }
