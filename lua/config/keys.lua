41949e0eecb4341972055e150d08dc168920805e
dendrocat <dan.re0@mail.ru>
2026-02-27 15:00:09 +0300

    Changing keymap for buffer removing

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -15,13 +15,23 @@ vim.keymap.set('n', ']d', function() jump(1) end, { desc = 'Next diagnostic' })
 -- Settings
 vim.keymap.set('n', '<leader>s', ':e $MYVIMRC | cd %:p:h | split . | wincmd k | pwd<CR>', { desc = "Open settings" })
 
-vim.keymap.set('n', '<leader>bd',
-	function()
-		for _, buf in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do
-			if (buf.changed == 0) then
-				vim.cmd('silent! bd! ' .. buf.bufnr)
-			end
+vim.keymap.set('n', '<leader>bd', function()
+	-- Check for modified buffers
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_valid(buf)
+			and vim.api.nvim_get_option_value('modified', { buf = buf })
+			and vim.api.nvim_get_option_value('buflisted', { buf = buf }) then
+			vim.notify("Found modified buffers. Save or discard changes before clearing buffers", vim.log.levels.ERROR)
+			return
 		end
-	end,
-	{ desc = "Buffer: all safe delete" }
-)
+	end
+
+	vim.cmd [[NvimTreeClose]]
+
+	-- if current buffer isn't [No Name]
+	if vim.fn.expand("%:p"):len() > 0 then
+		vim.cmd [[%bd|e#|bd#]] -- delete all buffers
+	end
+
+	vim.cmd [[NvimTreeFocus]]
+end, { desc = "Buffer: all delete", noremap = true, silent = true })
